797e0fb26420d2ad1f4aab60a504a151c3454028
Maxim Nakonechnyy <maxim.n.dev@gmail.com>
2024-06-30 20:57:49 +0200

    use patch instead of navigate

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -45,13 +45,21 @@ defmodule SlaxWeb.ChatRoomLive do
   def mount(params, _session, socket) do
     rooms = Repo.all(Room)
 
+    {:ok, assign(socket, rooms: rooms)}
+  end
+
+  @spec handle_params(map(), any(), Phoenix.LiveView.Socket.t()) :: {:noreply, any()}
+  def handle_params(params, _session, socket) do
+    IO.puts("handle_params #{inspect(params)} (connected: #{connected?(socket)})")
+
     room =
       case Map.fetch(params, "id") do
         {:ok, id} -> Repo.get!(Room, id)
-        :error -> List.first(rooms)
+        :error -> List.first(socket.assigns.rooms)
       end
 
-    {:ok, assign(socket, hide_topic?: false, room: room, rooms: rooms)}
+
+      {:noreply, assign(socket, hide_topic?: false, room: room)}
   end
 
   def handle_event("toggle-topic", _params, socket) do
@@ -63,18 +71,18 @@ defmodule SlaxWeb.ChatRoomLive do
 
   defp room_link(assigns) do
     ~H"""
-    <a
+    <.link
       class={[
         "flex items-center h-8 text-sm pl-8 pr-3",
         (@active && "bg-slate-300") || "hover:bg-slate-300"
       ]}
-      href={~p"/rooms/#{@room}"}
+      patch={~p"/rooms/#{@room}"}
     >
       <.icon name="hero-hashtag" class="h-4 w-4" />
       <span class={["ml-2 leading-none", @active && "font-bold"]}>
         <%= @room.name %>
       </span>
-    </a>
+    </.link>
     """
   end
 end
