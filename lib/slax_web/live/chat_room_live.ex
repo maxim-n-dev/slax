ffbbfedef9e14004c97f0cfa3abfd24fe5cee1ae
Maksym Yuriy Nakonechnyy <maksymyuriynakonechnyy@Mac-mini-de-Maksym.local>
2024-07-04 16:00:49 +0200

    add messages

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -2,11 +2,11 @@ defmodule SlaxWeb.ChatRoomLive do
   use SlaxWeb, :live_view
 
   alias Slax.Chat
-  alias Slax.Chat.Room
+  alias Slax.Chat.{Room, Message}
 
   def render(assigns) do
     ~H"""
-      <div class="flex flex-col flex-shrink-0 w-64 bg-slate-100">
+    <div class="flex flex-col flex-shrink-0 w-64 bg-slate-100">
       <div class="flex justify-between items-center flex-shrink-0 h-16 border-b border-slate-300 px-4">
         <div class="flex flex-col gap-1.5">
           <h1 class="text-lg font-bold text-gray-800">
@@ -31,7 +31,7 @@ defmodule SlaxWeb.ChatRoomLive do
             <.link
               class="font-normal text-xs text-blue-600 hover:text-blue-700"
               navigate={~p"/rooms/#{@room}/edit"}
-              >
+            >
               Edit
             </.link>
           </h1>
@@ -44,46 +44,49 @@ defmodule SlaxWeb.ChatRoomLive do
           </div>
         </div>
         <ul class="relative z-10 flex items-center gap-4 px-4 sm:px-6 lg:px-8 justify-end">
-      <%= if @current_user do %>
-        <li class="text-[0.8125rem] leading-6 text-zinc-900">
-          <%= @current_user.email %>
-        </li>
-        <li>
-          <.link
-            href={~p"/users/settings"}
-            class="text-[0.8125rem] leading-6 text-zinc-900 font-semibold hover:text-zinc-700"
-          >
-            Settings
-          </.link>
-        </li>
-        <li>
-          <.link
-            href={~p"/users/log_out"}
-            method="delete"
-            class="text-[0.8125rem] leading-6 text-zinc-900 font-semibold hover:text-zinc-700"
-          >
-            Log out
-          </.link>
-        </li>
-      <% else %>
-        <li>
-          <.link
-            href={~p"/users/register"}
-            class="text-[0.8125rem] leading-6 text-zinc-900 font-semibold hover:text-zinc-700"
-          >
-            Register
-          </.link>
-        </li>
-        <li>
-          <.link
-            href={~p"/users/log_in"}
-            class="text-[0.8125rem] leading-6 text-zinc-900 font-semibold hover:text-zinc-700"
-          >
-            Log in
-          </.link>
-        </li>
-      <% end %>
-    </ul>
+          <%= if @current_user do %>
+            <li class="text-[0.8125rem] leading-6 text-zinc-900">
+              <%= @current_user.email %>
+            </li>
+            <li>
+              <.link
+                href={~p"/users/settings"}
+                class="text-[0.8125rem] leading-6 text-zinc-900 font-semibold hover:text-zinc-700"
+              >
+                Settings
+              </.link>
+            </li>
+            <li>
+              <.link
+                href={~p"/users/log_out"}
+                method="delete"
+                class="text-[0.8125rem] leading-6 text-zinc-900 font-semibold hover:text-zinc-700"
+              >
+                Log out
+              </.link>
+            </li>
+          <% else %>
+            <li>
+              <.link
+                href={~p"/users/register"}
+                class="text-[0.8125rem] leading-6 text-zinc-900 font-semibold hover:text-zinc-700"
+              >
+                Register
+              </.link>
+            </li>
+            <li>
+              <.link
+                href={~p"/users/log_in"}
+                class="text-[0.8125rem] leading-6 text-zinc-900 font-semibold hover:text-zinc-700"
+              >
+                Log in
+              </.link>
+            </li>
+          <% end %>
+        </ul>
+      </div>
+      <div class="flex flex-col flex-grow overflow-auto">
+        <.message :for={message <- @messages} message={message} />
       </div>
     </div>
     """
@@ -97,15 +100,21 @@ defmodule SlaxWeb.ChatRoomLive do
 
   @spec handle_params(map(), any(), Phoenix.LiveView.Socket.t()) :: {:noreply, any()}
   def handle_params(params, _session, socket) do
-
     room =
       case Map.fetch(params, "id") do
         {:ok, id} -> Chat.get_room!(id)
         :error -> List.first(socket.assigns.rooms)
       end
 
+    messages = Chat.list_messages_in_room(room)
 
-      {:noreply, assign(socket, hide_topic?: false, page_title: "#" <> room.name, room: room)}
+    {:noreply,
+     assign(socket,
+       hide_topic?: false,
+       page_title: "#" <> room.name,
+       room: room,
+       messages: messages
+     )}
   end
 
   def handle_event("toggle-topic", _params, socket) do
@@ -129,6 +138,24 @@ defmodule SlaxWeb.ChatRoomLive do
         <%= @room.name %>
       </span>
     </.link>
+    """
+  end
+
+  attr :message, Message, required: true
+
+  defp message(assigns) do
+    ~H"""
+    <div class="relative flex px-4 py-3">
+      <div class="h-10 w-10 rounded flex-shrink-0 bg-slate-300"></div>
+        <div class="ml-2">
+          <div class="-mt-1">
+            <.link class="text-sm font-semibold hover:underline">
+              <span>User </span>
+            </.link>
+            <p class="text-sm"><%= @message.body %></p>
+          </div>
+      </div>
+    </div>
     """
   end
 end
