96283b3b5e9d3702ed36bf17ce0a7568e1110b56
Maksym Yuriy Nakonechnyy <maksymyuriynakonechnyy@Mac-mini-de-Maksym.local>
2024-07-07 13:19:29 +0200

    only show delete button on hover

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -224,10 +224,10 @@ defmodule SlaxWeb.ChatRoomLive do
 
   defp message(assigns) do
     ~H"""
-    <div id={@dom_id} class="relative flex px-4 py-3">
+    <div id={@dom_id} class=" group relative flex px-4 py-3">
       <button
         :if={@message.user.id == @current_user.id}
-        class="absolute top-4 right-4 text-red-500 hover:text-red-800 cursor-pointer"
+        class="absolute top-4 right-4 text-red-500 hover:text-red-800 cursor-pointer hidden group-hover:block"
         data-confirm="Are you sure?"
         phx-click="delete-message"
         phx-value-id={@message.id}
