aa254ba3cf893fe9cbcbaccfb5f0614de24b3b52
Maksym Yuriy Nakonechnyy <maksymyuriynakonechnyy@Mac-mini-de-Maksym.local>
2024-07-07 15:33:25 +0200

    hook added for scrolling chat view

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -91,7 +91,7 @@ defmodule SlaxWeb.ChatRoomLive do
           <% end %>
         </ul>
       </div>
-      <div id="room-messages" class="flex flex-col flex-grow overflow-auto" phx-update="stream">
+      <div phx-hook="RoomMessages" id="room-messages" class="flex flex-col flex-grow overflow-auto" phx-update="stream">
         <.message
           :for={{dom_id, message} <- @streams.messages}
           current_user={@current_user}
@@ -199,7 +199,12 @@ defmodule SlaxWeb.ChatRoomLive do
   end
 
   def handle_info({:new_message, message}, socket) do
-    {:noreply, stream_insert(socket, :messages, message)}
+    socket =
+      socket
+      |> stream_insert(:messages, message)
+      |> push_event("scroll_messages_to_bottom", %{})
+
+    {:noreply, socket}
   end
 
   def handle_info({:message_deleted, message}, socket) do
